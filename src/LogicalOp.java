c5a7850cddca0a28732d2805e55943b8bb7102f7
Simina <simina.dranca@yahoo.com>
2022-11-02 15:55:41 +0200

    Lab11IfElse

--- src/LogicalOp.java
+++ src/LogicalOp.java
@@ -8,13 +8,106 @@ public class LogicalOp {
         }
     }
 
-    public String isTextTheSame(String first1, String second2){
-    String first1 = "FastTrackBT";
-    String second2 = "FastTrackIT";
-
-           if ( first1.equals(second2) ) {
-        System.out.println("Learning text equivalence");
-    } else if ( !first1.equals(second2) ){
-       System.out.println("Got to try some more");}
+    public String textComparison(String textInput){
+        if(textInput.equals("FastTrackIT")){
+            return "Learning text comparison";
+        }else {
+            return "Got to try some more";
+        }
     }
-}
+    //Ex 6 snow
+    public String snowFall(int snow){
+        if (snow>8 || snow==6){
+
+            return "The amount of snow this winter was " + snow;
+
+        } else {
+            return "The forecast snow is (cm) " + snow;}
+
+    }
+
+    //Ex. 7
+    public String biggerEqualSmaller(int number) {
+
+        if (number > 3 && number != 4){
+            System.out.println("The number " + number + " is bigger than 3 and not equal to 4");
+        } else if (number == 4) {
+                System.out.println("The number " + number + " is equal to 4");
+            } else if (number < 3) {
+                System.out.println("The number " + number + " is smaller than 3");
+            }
+        return "!";}
+
+    public int SwitchCase (int numberInput) {
+    switch (numberInput) {
+        case 1:
+        System.out.println("Showing number: 1");
+        break;
+        case 2:
+            System.out.println("Showing number: 2");
+            break;
+        case 3:
+            System.out.println("Showing number: 3");
+            break;
+        case 4:
+            System.out.println("Showing number: 4");
+            break;
+        case 5:
+            System.out.println("Showing number: 5");
+            break;
+        case 6:
+            System.out.println("Showing number: 6");
+            break;
+        case 7:
+            System.out.println("Showing number: 7");
+            break;
+        case 8:
+            System.out.println("Showing number: 8");
+            break;
+        case 9:
+            System.out.println("Showing number: 9");
+            break;
+
+        default:
+            System.out.println("Invalid input - Wrong number.");
+            break;
+    }
+    return numberInput;
+
+    }
+/* 9. Creati o metoda care sa se numeasca isNumberEven. Metoda trebuie sa primeasca ca parametru un numar,
+ si sa returneze adevarat sau fals. Daca numarul e par sa returneze true iar daca e impar sa returnese false.
+  Apelati metoda in main() pentru a verifica daca functioneaza.
+ */
+    public boolean isNumberEven(int myNumber) {
+        if (myNumber % 2 == 0) {
+            return true;
+        } else {
+           return false; }
+    }
+   // 10. Creati o metoda care sa se numeasca isEligibleToVote. Metoda sa primeasca ca si parametru un numar,
+    // care sa reprezinte varsta, iar metoda sa iti zica daca daca persoana are drept de vot.
+    // Daca varsta e peste 18, sa se returneze true iar daca nu, sa returneze false.
+    // Apelati metoda in main() pentru a verifica daca functioneaza.
+
+     public boolean isEligibleToVote(int age) {
+        if (age < 18) {
+            return false;
+        }else {
+            return true;
+        }
+     }
+//11. Creati o metoda care sa primeasca 3 numere ca si parametrii si sa returneze cel mai mare numar.
+// Apelati metoda in main() pentru a verifica daca functioneaza.
+
+    public int biggestNumber(int first, int second, int third) {
+        if (first >= second && first >= third) {
+            return first;
+        } else if (second >= first && second >= third) {
+            return second;
+        }else{
+            return third;
+        }
+    }
+    }
+
--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,6 @@ public class Main {
         double y = 120;
         int xx = 12;
         int yy = 24;
-        String myString = "FastTrackIT";
-        String myString2 = "girl, please";
 
 
         //object creating (an instance of Calculator class)
@@ -33,10 +31,34 @@ public class Main {
         System.out.println("modulo is " + myReminder);
 
         LogicalOp myOPResult = new LogicalOp();
-        int biggest = myOPResult.checkBiggerNumber(xx,yy);
-        System.out.println("The bigger number is " + myOPResult );
+        int biggest = myOPResult.checkBiggerNumber(xx, yy);
+        System.out.println("The bigger number is " + biggest);
 
-    String isItNow = myOPResult.isTextTheSame(myString, myString2);
-    System.out.println(myOPResult);
+        //text comparison Ex. 5 tema
+    String isItNow = "FastTrackIT";
+    System.out.println(myOPResult.textComparison(isItNow));
+
+     //snow fall Ex 6 tema
+
+        System.out.println(myOPResult.snowFall(13));
+
+    //number equivalence Ex 7
+        System.out.println(myOPResult.biggerEqualSmaller(10));
+
+//Showing number Ex. 8 tema
+        System.out.println(myOPResult.SwitchCase(11));
+
+        //isNumberEven Ex. 9 tema
+        System.out.println(myOPResult.isNumberEven(11));
+
+        // isEligibleToVote Ex 10 tema
+        System.out.println(myOPResult.isEligibleToVote(11));
+
+        //biggest number out of given 3 ex 11 tema
+        System.out.println(myOPResult.biggestNumber(33, 18, 17));
+
     }
+
+
 }
+
